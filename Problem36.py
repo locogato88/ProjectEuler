137a69eecc4ec96cc731958d701e1ed8c224a2d3
locogato88 <narvaezster@gmail.com>
2017-03-27 20:29:06 -0500

    Update Problem36.py

--- Problem36.py
+++ Problem36.py
@@ -1,13 +1,13 @@
-
+import time
+start = time.time()
 count = 0
-for i in range(1000000):
+for i in range(1000000,2):
     i = str(i)
     if(i == i[::-1]):
         b = str("{0:b}".format(int(i)))
         if(b == b[::-1]):
             count +=int(i)
-            print(i)
 
 
 print (count)
-        
+print ("this took", time.time()-start)
